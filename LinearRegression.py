dacdd236426d21ca6f97aa47ab9e3e39085e2346
privateos <privateos@foxmail.com>
2020-09-22 18:01:57 +0800

    divide to three function

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 def train(X, Y, w, b):
+    print("manual derivative")
     n, d = X.shape
     _, m = Y.shape
     # w = np.random.randn(d, m)
     # b = np.random.randn(m)
 
     alpha = 0.01
-    epochs = 10
+    epochs = 3
     for i in range(epochs):
         Y_hat = np.dot(X, w) + b
         z = (Y_hat - Y)**2
@@ -40,45 +41,57 @@ np_w_copy = np.copy(np_w)
 np_b_copy = np.copy(np_b)
 train(X, Y, np_w, np_b)
 
-print('start pytorch')
 import torch
 import torch.nn as nn
 import torch.optim as optim
+def autotrain(X, Y, w, b):
+    print('autograd')
 
-epochs=10
-lr = 0.01
-X = torch.from_numpy(X).float()
-Y = torch.from_numpy(Y).float()
+    epochs=3
+    lr = 0.01
+    X = torch.from_numpy(X).float()
+    Y = torch.from_numpy(Y).float()
 
-w = torch.autograd.Variable(torch.from_numpy(np_w_copy).float(), requires_grad=True)
-b = torch.autograd.Variable(torch.from_numpy(np_b_copy).float(), requires_grad=True)
-X = torch.autograd.Variable(X)
-Y = torch.autograd.Variable(Y)
+    w = torch.autograd.Variable(torch.from_numpy(w).float(), requires_grad=True)
+    b = torch.autograd.Variable(torch.from_numpy(b).float(), requires_grad=True)
+    X = torch.autograd.Variable(X)
+    Y = torch.autograd.Variable(Y)
 
-for i in range(epochs):
-    Y_hat = torch.matmul(X, w) + b
-    z = (Y_hat - Y)**2
-    A = torch.sum(z, dim=1)
-    B = torch.mean(A, dim=0)
+    for i in range(epochs):
+        Y_hat = torch.matmul(X, w) + b
+        z = (Y_hat - Y)**2
+        A = torch.sum(z, dim=1)
+        B = torch.mean(A, dim=0)
 
-    B.backward()
-    print('loss=' + str(B.item()))
-    print(w.grad, b.grad)
+        B.backward()
+        print('loss=' + str(B.item()))
+        print(w.grad, b.grad)
 
-    w.data = w.data - lr * w.grad.data
-    b.data = b.data - lr* b.grad.data
-    w.grad.data.zero_()
-    b.grad.data.zero_()
-    input()
-# model = nn.Linear(X.shape[1], Y.shape[1])
-# mse = nn.MSELoss()
+        w.data = w.data - lr * w.grad.data
+        b.data = b.data - lr* b.grad.data
+        w.grad.data.zero_()
+        b.grad.data.zero_()
+        input()
+
+def torchtrain(X, Y):
+    print('torch')
+    X = torch.from_numpy(X).float()
+    Y = torch.from_numpy(Y).float()
+    epochs=3
+    lr=0.01
+    model = nn.Linear(X.shape[1], Y.shape[1])
+    mse = nn.MSELoss()
 
 
-# optimizer = optim.SGD(model.parameters(), lr=0.001)
-# for i in range(epochs):
-#     Y_hat = model(X)#X@W + b
-#     B = mse(Y, Y_hat)
-#     optimizer.zero_grad()
-#     B.backward()#反向求导
-#     optimizer.step()#更新参数
-#     print('loss = ' + str(B.item()))
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+    for i in range(epochs):
+        Y_hat = model(X)#X@W + b
+        B = mse(Y, Y_hat)
+        optimizer.zero_grad()
+        B.backward()#反向求导
+        optimizer.step()#更新参数
+        print('loss = ' + str(B.item()))
+        input()
+
+autotrain(X, Y, np_w_copy, np_b_copy)
+torchtrain(X, Y)
